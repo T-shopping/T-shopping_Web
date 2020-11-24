52bf15a3a412ad7b6c53feadc513840d517e5945
YeongG <yeonggil223@gmail.com>
2020-11-24 13:46:26 +0900

    [MODIFY] Main component/style

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import * as G from "../../GlobalStyle";
 
 export const Container = styled.div`
-  ${G.WitdhDiv} {
+  > ${G.WitdhDiv} {
     height: calc(100vh - 150px);
     display: flex;
     justify-content: center;
@@ -11,10 +11,20 @@ export const Container = styled.div`
     min-height: 800px;
     box-sizing: border-box;
   }
+
+  height: 100vh;
+  width: 100%;
+
+  min-width: 1800px;
+  min-height: 900px;
 `;
 
+export const RightContent = styled.div`
+  flex: 1;
+`;
 export const Bar = styled.div`
   height: 85%;
+  margin-right: 30px;
   margin-top: 30px;
   background-color: white;
   border-right: 1px solid white;
@@ -44,14 +54,78 @@ export const MoreDiv = styled.div`
   font-size: 20px;
   text-align: right;
   width: 100%;
+  margin-bottom: 20px;
+`;
+
+export const GroupWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 40px;
+`;
+
+export const SubBackground = styled.div`
+  flex: 1;
+  width: 100%;
+`;
+
+export const BackgroundTop = styled(SubBackground)``;
+export const BackgroundBottom = styled(SubBackground)`
+  background: linear-gradient(#e4e4e4, white);
+`;
+
+export const MyGroupHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const GroupName = styled.span`
+  font-size: 28px;
+`;
+
+export const MyGroupWrap = styled.div`
+  width: 70%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  padding: 90px;
+  background-color: white;
+  box-sizing: border-box;
+
+  ${MoreDiv} {
+    width: unset !important;
+  }
+`;
+export const BlankHeight = styled.div`
+  height: 250px;
+  width: 100%;
+  background-color: white;
 `;
 
 export const ProductWrap = styled.div`
   display: flex;
   flex: 1;
-  margin-left: 50px;
   height: 100%;
   align-items: center;
   justify-content: space-between;
   width: 100%;
+`;
+
+export const PlusFooter = styled.div`
+  height: 200px;
+  background-color: #515151;
+
+  > div {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+export const GroupContainer = styled(Container)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  position: relative;
 `;
