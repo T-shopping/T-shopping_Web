f42af9f4215e81edabb5674db951f36d7ef90b73
YeongG <yeonggil223@gmail.com>
2020-11-24 17:34:10 +0900

    [MODIFY] asset/component/container index.ts

---
+++ src/containers/index.ts
@@ -0,0 +1,4 @@
+export { default as MainContainer } from "./Main/MainContainer";
+export { default as LoginContainer } from "./Login/LoginContainer";
+export { default as RegisterContainer } from "./Register/RegisterContainer";
+export { default as BoardWriteContainer } from "./BoardWrite/BoardWriteContainer";
