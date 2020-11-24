e6845190ecf8fbb9babbf32ccc6758893dc1212a
YeongG <yeonggil223@gmail.com>
2020-11-24 10:09:36 +0900

    [CREATE] ProductCategoryItem component/style

---
+++ src/components/ProductCategory/ProductCategoryItem/styles.ts
@@ -0,0 +1,27 @@
+import styled, { css } from "styled-components";
+
+export const Container = styled.div<{ isActive: boolean }>`
+  font-size: 28px;
+  width: 230px;
+  color: white;
+  display: flex;
+  margin-top: 15px;
+  padding: 12px 6px;
+  justify-content: space-evenly;
+  margin-right: 30px;
+
+  & div:nth-child(1) {
+    font-size: 20px;
+  }
+
+  & div:nth-child(2) {
+    font-size: 17px;
+  }
+
+  ${props =>
+    props.isActive &&
+    css`
+      background-color: white;
+      color: blue;
+    `}
+`;
