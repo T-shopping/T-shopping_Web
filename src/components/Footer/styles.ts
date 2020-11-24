f44d8dceb65a6d3a4d1c7676bfc7e367c313413f
YeongG <yeonggil223@gmail.com>
2020-11-24 13:44:51 +0900

    [CREATE] Footer component/style

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #515151;
+  color: white;
+  height: 326px;
+
+  display: flex;
+
+  justify-content: center;
+  align-items: center;
+`;
