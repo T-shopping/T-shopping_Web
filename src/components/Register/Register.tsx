f471d5974106069e63cd4d4f815e05f8ef382614
YeongG <yeonggil223@gmail.com>
2020-11-25 22:52:34 +0900

    [MODIFY] Register component

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,9 +1,51 @@
-import React, { FC } from "react";
+import React, { ChangeEvent, FC } from "react";
+import { useCallback } from "react";
+import { useState } from "react";
 import { loginBackground, logoPng } from "../../assets";
+import { requestApi } from "../../lib/api";
 import { HelpUrl } from "../Login/styles";
 import * as S from "./styles";
 
+interface Register {
+  email: string;
+  password: string;
+  passwordRe: string;
+}
+
 const Register: FC = () => {
+  const [registerInfo, setRegisterInfo] = useState<Register>({
+    email: "",
+    password: "",
+    passwordRe: ""
+  });
+
+  const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setRegisterInfo(prev => ({ ...prev, [name]: value }));
+  }, []);
+
+  const register = useCallback(() => {
+    const { email, password, passwordRe } = registerInfo;
+    if (!email || !password || !passwordRe) {
+      alert("빈칸을 채워주세요");
+      return;
+    }
+    if (password !== passwordRe) {
+      alert("비밀번호와 재입력이 다릅니다");
+      return;
+    }
+    requestApi("post", "/api/auth/register", {
+      email,
+      password
+    })
+      .then(res => {
+        alert("계정이 생성되었습니다");
+      })
+      .catch(() => {
+        alert("이미 있습니다");
+      });
+  }, [registerInfo]);
+
   return (
     <S.Container>
       <S.RegisterModal>
@@ -15,15 +57,30 @@ const Register: FC = () => {
             </S.Logowrap>
             <S.InputWrap>
               이메일
-              <S.RegisterInput />
+              <S.RegisterInput
+                type="email"
+                onChange={onChange}
+                name="email"
+                value={registerInfo.email}
+              />
               비밀번호
-              <S.RegisterInput />
+              <S.RegisterInput
+                type="password"
+                onChange={onChange}
+                name="password"
+                value={registerInfo.password}
+              />
               비밀번호 확인
-              <S.RegisterInput />
+              <S.RegisterInput
+                type="password"
+                onChange={onChange}
+                name="passwordRe"
+                value={registerInfo.passwordRe}
+              />
             </S.InputWrap>
           </S.RegisterFormHeader>
           <S.RegisterFormBody>
-            <S.RegisterSubmitBtn>다음</S.RegisterSubmitBtn>
+            <S.RegisterSubmitBtn onClick={register}>다음</S.RegisterSubmitBtn>
             <HelpUrl>
               회원이신가요? <S.LoginText to="/login">로그인</S.LoginText>
             </HelpUrl>
