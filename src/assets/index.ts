789ed3bebaf2d61126de175f67f60a3e829cdc7f
YeongG <yeonggil223@gmail.com>
2020-11-25 22:40:03 +0900

    [MODIFY] asset index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,8 @@
 export { default as groupImg } from "./groupImg.png";
 export { default as logoPng } from "./logo.png";
 export { default as loginBackground } from "./loginBackground.png";
+export { default as contentBoxIcon } from "./contentBoxIcon.png";
+export { default as productPersonIcon } from "./productPersonIcon.png";
+export { default as plusBtn } from "./plusBtn.svg";
+export { default as X } from "./X.svg";
+export { default as king } from "./king.svg";
