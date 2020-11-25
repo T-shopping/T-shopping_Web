03fdbd21bfd325890732eb8ab788c9f76aff1586
YeongG <yeonggil223@gmail.com>
2020-11-25 22:47:21 +0900

    [CREATE] Product container

---
+++ src/containers/Product/ProductContainer.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import { ProductPage } from "../../components";
+
+const ProductContainer: FC = () => {
+  return <ProductPage />;
+};
+
+export default ProductContainer;
