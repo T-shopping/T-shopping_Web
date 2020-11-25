487cb52a2dc891e9ab381a61c34a2edbdf90fd8b
YeongG <yeonggil223@gmail.com>
2020-11-25 22:39:35 +0900

    [MODIFY] App 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React, { FC } from "react";
-import { Switch, BrowserRouter, Route } from "react-router-dom";
-import { ShoppingBasket,Main, ProductPage,ProductCommunity } from "./components";
+import { Switch, BrowserRouter, Route, useHistory } from "react-router-dom";
+import { plusBtn } from "./assets";
 import {
   LoginContainer,
   MainContainer,
   RegisterContainer,
-  BoardWriteContainer
+  BoardWriteContainer,
+  ProductContainer,
+  ProductDetailContainer,
+  ApplyNowContainer,
+  ApplyFinishContainer,
+  GroupContainer,
+  ReviewContainer,
+  QaContainer
 } from "./containers";
 import * as S from "./GlobalStyle";
 
@@ -15,13 +22,23 @@ const App: FC<{}> = () => {
       <S.GlobalStyle />
       <S.Container>
         <Switch>
-          <Route path="/product" component={ProductPage} />
-          <Route path="/review" component={ProductCommunity} />
-          <Route path="/shop" component={ShoppingBasket} />
           <Route exact path="/" component={MainContainer} />
+          <Route
+            exact
+            path="/product/list/:category"
+            component={ProductContainer}
+          />
+
+          <Route exact path="/qa" component={QaContainer} />
+          <Route exact path="/review" component={ReviewContainer} />
+          <Route exact path="/group" component={GroupContainer} />
+          <Route exact path="/apply/now" component={ApplyNowContainer} />
+          <Route exact path="/apply/finish" component={ApplyFinishContainer} />
+
+          <Route exact path="/product/:id" component={ProductDetailContainer} />
           <Route exact path="/login" component={LoginContainer} />
           <Route exact path="/register" component={RegisterContainer} />
-          <Route exact path="/write" component={BoardWriteContainer} />
+          <Route exact path="/write/post" component={BoardWriteContainer} />
         </Switch>
       </S.Container>
     </BrowserRouter>
