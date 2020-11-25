3e1e12e9b6fe9382560982fcafe0369b3ad6ccec
YeongG <yeonggil223@gmail.com>
2020-11-25 22:54:39 +0900

    [MODIFY] Login component

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,8 +1,25 @@
-import React, { FC } from "react";
+import React, { ChangeEvent, FC, useState } from "react";
+import { useCallback } from "react";
 import { logoPng, loginBackground } from "../../assets";
+import { requestApi } from "../../lib/api";
 import * as S from "./styles";
 
+interface Login {
+  email: string;
+  password: string;
+}
+
 const Login: FC = () => {
+  const [loginInfo, setLoginInfo] = useState<Login>({
+    email: "",
+    password: ""
+  });
+  const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setLoginInfo(prev => ({ ...prev, [name]: value }));
+  }, []);
+
+  const login = useCallback(() => {}, []);
   return (
     <S.Container>
       <S.LoginModla>
@@ -14,13 +31,22 @@ const Login: FC = () => {
             </S.LogoWrap>
             <S.InptWrap>
               아이디
-              <S.LoginInput />
+              <S.LoginInput
+                onChange={onChange}
+                value={loginInfo.email}
+                name="email"
+              />
               비밀번호
-              <S.LoginInput />
+              <S.LoginInput
+                onChange={onChange}
+                type="password"
+                value={loginInfo.password}
+                name="password"
+              />
             </S.InptWrap>
           </S.LoginFormHeader>
           <S.LoginFormBody>
-            <S.LoginSubmitBtn>로그인</S.LoginSubmitBtn>
+            <S.LoginSubmitBtn onClick={login}>로그인</S.LoginSubmitBtn>
             <S.HelpUrl>
               회원이 아니신가요?
               <S.RegisterText to="/register">회원가입</S.RegisterText>
